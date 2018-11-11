6925f756569e36de81df2d723a96bf7c860bb1ef
lucid.dion@gmail.com <lucid.dion@gmail.com>
2018-11-11 11:07:44 -0500

    Assembly version and minimum Q# 152

--- TASCExtensions/TASCExtensions/Properties/AssemblyInfo.cs
+++ TASCExtensions/TASCExtensions/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using QuantaculaCore;
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: MinimumQNumber(146)]
+[assembly: MinimumQNumber(152)]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -34,5 +34,5 @@ using QuantaculaCore;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.10.0")]
-[assembly: AssemblyFileVersion("1.0.10.0")]
+[assembly: AssemblyVersion("1.0.11.0")]
+[assembly: AssemblyFileVersion("1.0.11.0")]
